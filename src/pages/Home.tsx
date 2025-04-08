ed9bb87eaebbf1cfe28adb427d9a449d72e8862a
Supathi <r.supathi@gmail.com>
2025-04-08 08:52:57 +0530

    Pages moved to pages directory. Moved routes to src. Added Home component.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import LoginButton from "../features/auth/components/LoginButton";
+
+function HomePage() {
+	return(
+        <>
+		<h1>Home Page</h1>
+        <LoginButton />
+        </>
+	);
+}
+
+export default HomePage;
---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElement,
+    Route,
+} from 'react-router-dom';
